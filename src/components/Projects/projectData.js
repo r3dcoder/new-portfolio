1129a9aba97f3bb48783a284c728242d96c302b1
r3dcoder <sdmahfuz@gmail.com>
2024-05-16 15:42:01 +0100

    added projects

--- src/components/Projects/projectData.js
+++ src/components/Projects/projectData.js
@@ -3,57 +3,57 @@ import storygram from "../../Assets/Projects/storygram.webp";
 import englishjanala from "../../Assets/Projects/englishjanala.webp";
 import hurryup from "../../Assets/Projects/hurryup.jpeg";
 import taxrebate from "../../Assets/Projects/taxrebate.jpeg";
-import suicide from "../../Assets/Projects/suicide.png";
-import emotion from "../../Assets/Projects/emotion.png";
+import shoopy from "../../Assets/Projects/shoopy.png";
+import spam from "../../Assets/Projects/spam.png";
 
 const projectData = [
   {
     imgPath: storygram,
     isBlog: false,
-    title: "Chatify",
-    description: "Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages.",
-    ghLink: "https://github.com/soumyajit4419/Chatify",
-    demoLink: "https://chatify-49.web.app/",
+    title: "StoryGram",
+    description: "Storygram provides a powerful AI photo editor with thousands of templates and features like AI Photo Enhance, Magic Remove, Background Change, AI Retouch, and more. I developed the web version and admin panel using React, Next.js, TypeScript, and Tailwind CSS.",
+    ghLink: "",
+    demoLink: "https://www.storygram.app/",
   },
   {
     imgPath: englishjanala,
     isBlog: false,
-    title: "Bits-0f-C0de",
-    description: "My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown.",
-    ghLink: "https://github.com/soumyajit4419/Bits-0f-C0de",
-    demoLink: "https://blogs.soumya-jit.tech/",
+    title: "English Janala",
+    description: "English Janala [ইংলিশ জানালা] is a premier language learning app tailored for Bangladeshi learners. It offers a user-friendly interface and offline capabilities to improve English skills. I developed the admin panel of this app using Next.js, React, and Tailwind CSS.",
+    ghLink: "",
+    demoLink: "https://play.google.com/store/apps/details?id=com.englishjanala.app",
   },
   {
     imgPath: hurryup,
     isBlog: false,
-    title: "Editor.io",
-    description: "Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage",
-    ghLink: "https://github.com/soumyajit4419/Editor.io",
-    demoLink: "https://editor.soumya-jit.tech/",
+    title: "HurryUp",
+    description: "HurryUp is an e-commerce site tailored for the local area of Sylhet, Bangladesh. I developed this site using Vue, TypeScript, and Tailwind CSS to provide a seamless and efficient online shopping experience for the community.",
+    ghLink: "https://hurry-up.netlify.app/",
+    demoLink: "https://hurry-up.netlify.app/",
   },
   {
     imgPath: taxrebate,
     isBlog: false,
-    title: "Plant AI",
-    description: "Used the plant disease dataset from Kaggle and trained a image classifer model using 'PyTorch' framework using CNN and Transfer Learning with 38 classes of various plant leaves. The model was successfully able to detect diseased and healthy leaves of 14 unique plants. I was able to achieve an accuracy of 98% by using Resnet34 pretrained model.",
-    ghLink: "https://github.com/soumyajit4419/Plant_AI",
-    demoLink: "https://plant49-ai.herokuapp.com/",
+    title: "Tax Rebate",
+    description: "I've developed a user-friendly web application that simplifies the tax refund process. Users can rely on our platform for expert assistance and guidance on maximizing their refunds. Powered by React, TypeScript, Next.js, MySQL, and Tailwind CSS, our application offers a seamless experience for all users",
+    ghLink: "",
+    demoLink: "https://taxrebate4u.vercel.app/",
   },
   {
-    imgPath: suicide,
+    imgPath: shoopy,
     isBlog: false,
-    title: "Ai For Social Good",
-    description: "Using 'Natural Launguage Processing' for the detection of suicide-related posts and user's suicide ideation in cyberspace and thus helping in sucide prevention.",
-    ghLink: "https://github.com/soumyajit4419/AI_For_Social_Good",
-    demoLink: "",
+    title: "Shoopy Manager",
+    description: "Shopy-Manager is a versatile platform built with React, Next.js, TypeScript, and Tailwind CSS. It allows users to set up websites and mobile apps effortlessly using our curated templates.",
+    ghLink: "",
+    demoLink: "https://shoppy-manager.vercel.app/",
   },
   {
-    imgPath: emotion,
+    imgPath: spam,
     isBlog: false,
-    title: "Face Recognition and Emotion Detection",
-    description: "Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%. Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person.",
-    ghLink: "https://github.com/soumyajit4419/Face_And_Emotion_Detection",
-    demoLink: "",
+    title: "Spam Email Detection",
+    description: "In my research, I utilized Python along with libraries such as Matplotlib, TensorFlow, and others to analyze and compare machine learning models for spam email detection.",
+    ghLink: "",
+    demoLink: "https://colab.research.google.com/drive/1lYh_Wcd1PdxwawJso28_5cGuJr0ja68Q?usp=sharing#scrollTo=DUPJTZeZt6u2",
   },
 ];
 
