7d98e1e084fd3f0f5b05d3fb58565d8264ad0208
r3dcoder <sdmahfuz@gmail.com>
2024-06-27 15:45:37 +0100

    added meta data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,45 +4,55 @@ import homeLogo from "../../Assets/home-main.svg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
+import { Helmet } from "react-helmet";
 
 function Home() {
   return (
-    <section>
-      <Container fluid className="home-section" id="home">
-        <Particle />
-        <Container className="home-content">
-          <Row>
-            <Col md={7} className="home-header">
-              <h1 style={{ paddingBottom: 15 }} className="heading">
-                Hi There!{" "}
-                <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻
-                </span>
-              </h1>
+    <>
+      <Helmet>
+        <title>Home - Syed Hassan</title>
+        <meta name="description" content="Welcome to the home page of Syed Hassan's portfolio." />
+        <meta name="keywords" content="Syed Hassan, portfolio, projects, about, contact" />
+      </Helmet>
+      <section>
 
-              <h1 className="heading-name">
-                I'M
-                <strong className="main-name"> SYED HASSAN</strong>
-              </h1>
+        <Container fluid className="home-section" id="home">
+          <Particle />
+          <Container className="home-content">
+            <Row>
+              <Col md={7} className="home-header">
+                <h1 style={{ paddingBottom: 15 }} className="heading">
+                  Hi There!{" "}
+                  <span className="wave" role="img" aria-labelledby="wave">
+                    👋🏻
+                  </span>
+                </h1>
 
-              <div style={{ padding: 50, textAlign: "left" }}>
-                <Type />
-              </div>
-            </Col>
+                <h1 className="heading-name">
+                  I'M
+                  <strong className="main-name"> SYED HASSAN</strong>
+                </h1>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
-            </Col>
-          </Row>
+                <div style={{ padding: 50, textAlign: "left" }}>
+                  <Type />
+                </div>
+              </Col>
+
+              <Col md={5} style={{ paddingBottom: 20 }}>
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className="img-fluid"
+                  style={{ maxHeight: "450px" }}
+                />
+              </Col>
+            </Row>
+          </Container>
         </Container>
-      </Container>
-      <Home2 />
-    </section>
+        <Home2 />
+      </section>
+    </>
+
   );
 }
 
